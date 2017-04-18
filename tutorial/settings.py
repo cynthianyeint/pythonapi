8cbae400f6cd557d9be4c3d46db751a9ff882f05
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-04-18 15:32:08 +0800

    change db setting

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
     # }
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cxa_data',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'NAME': 'd18nagk2ds2qdb',
+        'USER': 'tftiawruckynry',
+        'PASSWORD': '1224df59dad968365e63a4c82ef61ae9bab64e237614d49a6b7723342ab4fa4d',
         'HOST': 'ec2-54-235-153-124.compute-1.amazonaws.com',
         'PORT': '5432',
     }
